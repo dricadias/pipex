548ddfda52101f41073b7f2294faff8b3d0e66ad
dricadias <andrielly.12dias.ad@gmail.com>
2025-04-19 16:13:08 +0100

    dealing with != than four args

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,15 @@
 /*   By: adias-do <adias-do@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 22:08:08 by adias-do          #+#    #+#             */
-/*   Updated: 2025/04/16 22:08:09 by adias-do         ###   ########.fr       */
+/*   Updated: 2025/04/19 16:11:29 by adias-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/pipex.h"
+
+// probably im gonna use envp too
+int	main(int argc, char **argv)
+{
+	if (argc != 5)
+		return (0); // a error message maybe ?
+}
