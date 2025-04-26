cafddf469342ecb36f05c4312fbc7410af3e1164
dricadias <89052954+dricadias@users.noreply.github.com>
2025-04-26 15:56:50 +0100

    more shell-like

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: adias-do <adias-do@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 22:08:08 by adias-do          #+#    #+#             */
-/*   Updated: 2025/04/26 05:22:56 by adias-do         ###   ########.fr       */
+/*   Updated: 2025/04/26 15:55:15 by adias-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	child_process(t_pipex *node, char **envp)
 {
 	if (!node->cmd1_path)
 	{
+		ft_putstr_fd("zsh: command not found: ", STDERR_FILENO);
 		ft_putstr_fd(node->cmd1_args[0], STDERR_FILENO);
-		ft_putstr_fd(": command not found", STDERR_FILENO);
 		ft_putstr_fd("\n", STDERR_FILENO);
 		free_pipex(NULL, node, 127);
 	}
@@ -38,8 +38,8 @@ void	child2_process(t_pipex *node, char **envp)
 {
 	if (!node->cmd2_path)
 	{
+		ft_putstr_fd("zsh: command not found: ", STDERR_FILENO);
 		ft_putstr_fd(node->cmd2_args[0], STDERR_FILENO);
-		ft_putstr_fd(": command not found", STDERR_FILENO);
 		ft_putstr_fd("\n", STDERR_FILENO);
 		free_pipex(NULL, node, 127);
 	}
